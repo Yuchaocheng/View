637de13460c6ae7e575c4784c51d910033a01a5f
yuchaocheng <yuchaocheng@hikvision.com.cn>
2021-06-24 07:15:32 +0800

    fit:Mustache基本信息学习完成

---
+++ Mustcache/template.js
@@ -0,0 +1,10 @@
+<ul id="list">
+    <li>
+        <div class="hd">小明的基本信息</div>
+        <div class="bd">
+            <p>姓名：小明</p>
+            <p>年龄：12</p>
+            <p>性别：男</p>
+        </div>
+    </li>
+</ul>
