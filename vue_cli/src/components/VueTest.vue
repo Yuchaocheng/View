9f28984153426fadaddbadc389c2f645d335a75a
yuchaocheng <yuchaocheng@hikvision.com.cn>
2021-09-03 09:00:53 +0800

    fit:Vue学习中期

---
+++ vue_cli/src/components/VueTest.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="VueTest">
+        <h1>vueTeset</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        debugger;
+    },
+};
+</script>
+
+<style scoped></style>
--- vue_cli/src/main.js
+++ vue_cli/src/main.js
@@ -1,16 +1,32 @@
 import Vue from "vue";
 import App from "./App.vue";
 import ElementUI from "element-ui";
+import VueRouter from "vue-router";
 import "element-ui/lib/theme-chalk/index.css";
 import "@/components/global/index.js";
+
+import VueTest from "@/components/VueTest.vue";
+import Components from "@/view/Components.vue";
+
 Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+const routes = [
+    { path: "/", component: Components },
+    { path: "/test", component: VueTest },
+];
+const router = new VueRouter({
+    routes, // (缩写) 相当于 routes: routes
+});
 
 Vue.config.productionTip = false;
 
 const App2 = Vue.extend(App);
-const vm =  new Vue({
+const vm = new Vue({
+    router,
     render: (h) => h(App),
 }).$mount("#app");
-console.log(App2.prototype.__proto__ === vm.__proto__);
-console.log(vm,"vm");
-console.log(vm.__proto__,"vm");
+
+// console.log(Vue, "Vue");
+// console.log(Vue.prototype, "Vue");
+// console.log(vm, "vm");
