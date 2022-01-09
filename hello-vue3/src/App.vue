49c1644a2a28ac436a5a98bf8cdbb41b86c68752
yuchaocheng <yuchaocheng@hikvision.com.cn>
2022-01-10 07:36:15 +0800

    feat: vue3新特性探索结束

--- hello-vue3/src/App.vue
+++ hello-vue3/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
+    <!-- v-model测试 -->
+    <v-model-test v-model="modelTitle" v-model:test="modelTest"></v-model-test>
     <!-- 组合式API语法糖 -->
-    <composition-sugar></composition-sugar>
+    <composition-sugar v-if="false"></composition-sugar>
     <!-- suspense组件测试 -->
     <suspense-test v-if="false"></suspense-test>
     <!-- teleport组件测试  -->
@@ -13,12 +15,13 @@
 </template>
 
 <script>
-import { provide, reactive } from 'vue';
+import { provide, reactive, ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 import setupTest from './components/setupTest.vue';
 import TeleportTest from './components/TeleportTest.vue';
 import SuspenseTest from './components/SuspenseTest.vue';
 import compositionSugar from './components/compositionSugar.vue';
+import vModelTest from './components/vModelTest.vue';
 
 export default {
   name: 'App',
@@ -28,15 +31,22 @@ export default {
     TeleportTest,
     SuspenseTest,
     compositionSugar,
+    vModelTest,
   },
   setup(props) {
     let car = reactive({ name: '奔驰', price: '40万' });
     provide('car', car);
     setTimeout(() => {
       car.price = 22222;
-    }, 10000);
+      modelTitle.value += ' 父组件改变值 ';
+      modelTest.value += ' 父组件改变test prop ';
+    }, 5000);
+    let modelTitle = ref('modelTitle');
+    let modelTest = ref('modelTest');
     return {
       car,
+      modelTitle,
+      modelTest,
     };
   },
 };
