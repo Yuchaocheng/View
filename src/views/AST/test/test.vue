eb0bc8bf1e8a3739d46dcded76dbbf26a3b32fb6
yuchaocheng <yuchaocheng@hikvision.com.cn>
2022-03-09 16:55:05 +0800

    feat：自动化路由注册复习

---
+++ src/views/AST/test/test.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>test</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,47 @@
-const path = require("path");
-const { VueLoaderPlugin } = require("vue-loader");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: "vue-loader",
-            },
-            {
-                test: /.js/,
-                loader:"babel-loader",
-                options:{
-                    plugins:["transform-vue-jsx"]
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|)$/,
-                type: "asset/resource",
-            },
-        ],
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-        }),
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, "dist"),
-        port: 8899,
-        compress: false,
-        open: true,
-    },
+  mode: 'development',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /.js/,
+        loader: 'babel-loader',
+        options: {
+          plugins: ['transform-vue-jsx']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|)$/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8899,
+    compress: false,
+    open: true
+  }
 };
